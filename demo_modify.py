724203117ef177a87e8b22bb6be97a23f723f56f
lamchonhang <joejoelam1112@gmail.com>
2023-11-29 00:34:22 +0800

    added demo_modify.py

---
+++ demo_modify.py
@@ -0,0 +1,82 @@
+import sys
+sys.path.append('core')
+
+import argparse
+import os
+import cv2
+import glob
+import numpy as np
+import torch
+from PIL import Image
+import time
+from raft import RAFT
+from utils import flow_viz
+from utils.utils import InputPadder
+# from utils.torch_utils import time_synchronized
+
+DEVICE = 'cuda'
+
+def load_image(imfile):
+    img = np.array(Image.open(imfile)).astype(np.uint8)
+    img = torch.from_numpy(img).permute(2, 0, 1).float()
+    return img[None].to(DEVICE)
+
+
+def viz(img, flo):
+    img = img[0].permute(1,2,0).cpu().numpy()
+    flo = flo[0].permute(1,2,0).cpu().numpy()
+    
+    # map flow to rgb image
+    flo = flow_viz.flow_to_image(flo)
+    img_flo = np.concatenate([img, flo], axis=0)
+
+    import matplotlib.pyplot as plt
+    plt.imshow(img_flo / 255.0)
+    plt.show(block=False)
+    plt.pause(1)
+    # cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
+    # cv2.waitKey()
+
+
+def demo(args):
+    model = torch.nn.DataParallel(RAFT(args))
+    model.load_state_dict(torch.load(args.model))
+
+    model = model.module
+    model.to(DEVICE)
+    model.eval()
+
+    with torch.no_grad():
+        images = glob.glob(os.path.join(args.path, '*.png')) + \
+                 glob.glob(os.path.join(args.path, '*.jpg'))
+        
+        images = sorted(images)
+        
+        img_flo_arr = []
+        for imfile1, imfile2 in zip(images[:-1], images[1:]):
+            image1 = load_image(imfile1)
+            image2 = load_image(imfile2)
+
+            padder = InputPadder(image1.shape)
+            image1, image2 = padder.pad(image1, image2)
+            t1 = time.time()
+            flow_low, flow_up = model(image1, image2, iters=1, test_mode=True)
+            torch.cuda.synchronize()
+            t2 = time.time()
+            print("--- %s seconds ---" % (t2-t1))
+            img_flo_arr.append((image1, flow_up))
+        
+        for i in range(len(img_flo_arr)):
+            viz(img_flo_arr[i][0], img_flo_arr[i][1])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help="restore checkpoint")
+    parser.add_argument('--path', help="dataset for evaluation")
+    parser.add_argument('--small', action='store_true', help='use small model')
+    parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
+    parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
+    args = parser.parse_args()
+
+    demo(args)
